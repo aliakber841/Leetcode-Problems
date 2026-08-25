94ccb0b84d0aeb824a0ff612d5410c8a906f8930
Ali Akbar <94167678+aliakber841@users.noreply.github.com>
2026-08-25 09:26:58 +0500

    Clean up recoverOrder method by removing comments
    Removed commented-out code for clarity.

--- 3668-restore-finishing-order.py
+++ 3668-restore-finishing-order.py
@@ -5,3 +5,14 @@ class Solution(object):
             if order[i] in friends:
                 result.append(order[i])
         return result
+
+# class Solution(object):
+#     def recoverOrder(self, order, friends):
+#         hash_table={}
+#         result=[]
+#         for i in range(len(friends)):
+#             hash_table[friends[i]]=i
+#         for j in range(len(order)):
+#             if order[j] in hash_table:
+#                 result.append(order[j])
+#         return result
